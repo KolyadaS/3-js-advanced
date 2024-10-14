e6ee74716271a92aa3353f4323657d882e6ef04d
Svetlana Kolyada <pfotka@yandex.ru>
2024-10-14 17:11:10 +0400

    1-map

--- 1-map/app.js
+++ 1-map/app.js
@@ -1,1 +1,25 @@
+/* Необходимо уникализировать массив, 
+содержащий объекты с идентичными идентификаторами 
+(например, ID1, ID2, повторно ID1), удаляя 
+повторяющиеся идентификаторы.*/
 
+let arr = [
+  { id: 1, name: "Вася" },
+  { id: 2, name: "Петя" },
+  { id: 1, name: "Вася" },
+  { id: 1, name: "Маша" },
+  { id: 3, name: "Катя" },
+];
+console.log(arr);
+
+const myMap = new Map();
+const mySet = new Set();
+
+arr.map((elem) => {
+  if (!myMap.has(elem.id)) {
+    myMap.set(elem.id, elem.name);
+    mySet.add(elem.name);
+  }
+});
+console.log(myMap);
+console.log(mySet);
