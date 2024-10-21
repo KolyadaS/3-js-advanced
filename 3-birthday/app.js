230ce659c71650684d9b31489b056885dff9446c
Svetlana Kolyada <pfotka@yandex.ru>
2024-10-21 16:36:27 +0400

    сокращение кол-ва строк кода

--- 3-birthday/app.js
+++ 3-birthday/app.js
@@ -1,18 +1,14 @@
 "use strict";
 
 const userBirthday1 = "2010-10-10";
-const userBirthday2 = "2010-10-16";
+const userBirthday2 = "2010-10-21";
 const userBirthday3 = "2010-10-30";
 
 function is14(stringDate) {
   const date = new Date(stringDate);
   const datePlus14 = date.setFullYear(date.getFullYear() + 14);
   const now = new Date();
-  if (datePlus14 > now) {
-    return false;
-  } else {
-    return true;
-  }
+  return !(datePlus14 > now);
 }
 
 console.log("Достиг ли пользователь полных 14 лет?");
